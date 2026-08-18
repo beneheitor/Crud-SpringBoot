1c67c3488b9c9f7502f8335381c435c9f94810d8
linus <linus@torvalds>
2026-08-18 09:21:13 -0300

    Createad Users class and User Controller

---
+++ src/main/java/br/com/cadastro/sentry/UserController.java
@@ -0,0 +1,11 @@
+package br.com.cadastro.sentry;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+    @GetMapping
+    public
+}
