deb29c5fe15b40bbb662f49701bb857066f95620
linus <linus@torvalds>
2026-08-18 10:12:08 -0300

    Usando notação para criar Getters/Setters e os constrtuores e retornando um usuario com o Get

--- src/main/java/br/com/cadastro/sentry/UserController.java
+++ src/main/java/br/com/cadastro/sentry/UserController.java
@@ -1,5 +1,6 @@
 package br.com.cadastro.sentry;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,5 +8,8 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping
-    public
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Users getUser(){
+        return new Users("Heitor","999.999.999-10","pedroberingeladuGRAU@gemeiu.com");
+    }
 }
--- src/main/java/br/com/cadastro/sentry/Users.java
+++ src/main/java/br/com/cadastro/sentry/Users.java
@@ -1,41 +1,19 @@
 package br.com.cadastro.sentry;
 
+import lombok.*;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+//@EqualsAndHashCode
+//@Data
 public class Users {
     private String nome;
     private String cpf;
     private String email;
 
-    public Users() {
-    }
-
-    public Users(String nome, String cpf, String email) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.email = email;
-    }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
