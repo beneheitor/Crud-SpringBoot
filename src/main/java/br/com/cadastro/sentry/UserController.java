82cd7bd437da7ec8ab27f09c9a870154ae0d3041
linus <linus@torvalds>
2026-08-18 11:51:15 -0300

    Create Put and Delete

--- src/main/java/br/com/cadastro/sentry/UserController.java
+++ src/main/java/br/com/cadastro/sentry/UserController.java
@@ -12,16 +12,37 @@ public class UserController {
 
     List<Users> registeredUsers = new ArrayList<>();
 
+    //GET PARA RETORNAR TODOS OS USUÁRIOS
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Users> findAllUsers(){
         return registeredUsers;
     }
 
+    //GET PARA RETORNAR APENAS UM USUÁRIO
+    @GetMapping("/{id}")
+    public Users findOneUser(@PathVariable int id){
+        return registeredUsers.get(id);
+    }
+
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Users addUser(@RequestBody Users user){
         registeredUsers.add(user);
         return registeredUsers.getLast();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Users updateUser(@PathVariable int id, @RequestBody Users user){
+        registeredUsers.set(id,user);
+        return registeredUsers.get(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable int id){
+            registeredUsers.remove(id);
     }
 }
