9f425bfadd165824094343ab711f1c5f55af65e9
linus <linus@torvalds>
2026-08-19 10:14:27 -0300

    Atualização: Fazendo CRUD com JPA

--- src/main/java/br/com/cadastro/sentry/UserController.java
+++ src/main/java/br/com/cadastro/sentry/UserController.java
@@ -1,48 +1,71 @@
 package br.com.cadastro.sentry;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/v1/users")
 public class UserController {
 
-    List<Users> registeredUsers = new ArrayList<>();
+
+    private final UserRepository userRepository;
+
+//    public UserController(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
 
     //GET PARA RETORNAR TODOS OS USUÁRIOS
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<Users> findAllUsers(){
-        return registeredUsers;
+    public List<Users> findAllUsers() {
+        return userRepository.findAll();
     }
 
     //GET PARA RETORNAR APENAS UM USUÁRIO
     @GetMapping("/{id}")
-    public Users findOneUser(@PathVariable int id){
-        return registeredUsers.get(id);
+    public Users findOneUser(@PathVariable UUID id) {
+        Optional<Users> findedUser = userRepository.findById(id);
+        if (findedUser.isPresent()){
+            return findedUser.get();
+        } else {
+            throw new RuntimeException("Usuário não encontrado!");
+        }
     }
 
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Users addUser(@RequestBody Users user){
-        registeredUsers.add(user);
-        return registeredUsers.getLast();
+    public Users addUser(@RequestBody Users user) {
+       return userRepository.save(user);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Users updateUser(@PathVariable int id, @RequestBody Users user){
-        registeredUsers.set(id,user);
-        return registeredUsers.get(id);
+    public Users updateUser(@PathVariable UUID id, @RequestBody Users user) {
+        Optional<Users> userSaved = userRepository.findById(id);
+        if (userSaved.isPresent()){
+            Users userUpdated = userSaved.get();
+            userUpdated.setNome(user.getNome());
+            userUpdated.setCpf(user.getCpf());
+            userUpdated.setEmail(user.getEmail());
+            return userRepository.save(userUpdated);
+        } else{
+            throw new RuntimeException("Usuário não encontrado com o ID: " + id);
+        }
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(@PathVariable int id){
-            registeredUsers.remove(id);
+    public void deleteUser(@PathVariable int id) {
+        registeredUsers.remove(id);
     }
 }
