07116abf10c458389f3fd4c398dcb0694cd65c62
linus <linus@torvalds>
2026-08-25 09:11:43 -0300

    Atualização: Adicionando metódo para encontrar usuário através do nome

--- src/main/java/br/com/cadastro/sentry/UserRepository.java
+++ src/main/java/br/com/cadastro/sentry/UserRepository.java
@@ -1,5 +1,6 @@
 package br.com.cadastro.sentry;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,5 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<Users, UUID> {
 
+    Users findByName(String name);
 }
