9eb0861a8fa5be2bfb7a701773d8305fd3a6f8a2
linus <linus@torvalds>
2026-08-25 08:52:09 -0300

    Atualização: Get, Put, Post e delete prontos, utilizando o repository

--- src/main/java/br/com/cadastro/sentry/UserController.java
+++ src/main/java/br/com/cadastro/sentry/UserController.java
@@ -1,7 +1,7 @@
 package br.com.cadastro.sentry;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
